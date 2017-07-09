ae593599cf81366764cab253c1c6be6c848623fd
root <root@localhost.localdomain>
2017-07-10 06:00:07 +0800

    toy scripts updated

---
+++ fileManip.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.security.*;
+
+
+public class fileManip{
+    String filename = "";
+    public fileManip(String filename){
+        this.filename = filename;
+    }
+
+    public String fileRead(){
+        String fstr = "";
+        try{
+            FileInputStream fi = new FileInputStream(filename);
+            int fSize = fi.available();
+            for (int i = 0; i < fSize; i++){
+                fstr += (char)fi.read();
+            }
+            fi.close();
+        }catch(Exception e){
+            System.err.println("An error occured at fileRead() function for \""+filename+"\"");
+        }
+        return fstr;
+    }
+    
+    public void fileWrite(String strCont){
+        try{
+            FileOutputStream fo = new FileOutputStream(filename);
+            char[] strContArr = strCont.toCharArray();
+            for (int i = 0; i < strContArr.length; i++){
+                fo.write(strContArr[i]);
+            }
+            fo.close();
+        }catch(Exception e){ 
+            System.err.println("An error occured at fileWrite() function for \""+filename+"\"");
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.security.*;
+
+
+public class test_main{
+    public static String[] splitAt(String str, String tok){
+        char[] strArr = str.toCharArray();
+        char[] tokArr = tok.toCharArray();
+        String[] rstArr = new String[strArr.length];
+        char[] tempArr = new char[tokArr.length];
+
+        int splitStartIdx, dplitEndIdx;
+        for(int i = 0; i < strArr.length-tokArr.length; i++){
+            splitStartIdx = i;
+            for (int j = 0; j < tokArr.length; j++){
+                i += j;
+                if (tokArr[j] != strArr[i]){
+                    break;
+                }
+                if (j == tokArr.length-1){
+                    splitEndIdx = i;
+                     
+                }
+            }
+        }
+    }
+
+
+
+}
+
+
+
